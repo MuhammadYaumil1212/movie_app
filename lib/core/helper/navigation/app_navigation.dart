23630220ff27aeee961475d268b05a9c6d6e0f21
yaumil <yaumilramkhadani@gmail.com>
2025-02-12 17:12:02 +0700

    change animation

--- lib/core/helper/navigation/app_navigation.dart
+++ lib/core/helper/navigation/app_navigation.dart
@@ -5,13 +5,18 @@ class AppNavigator {
   static void pushReplacement(BuildContext context, Widget widget) {
     Navigator.of(context).pushReplacement(
       PageRouteBuilder(
-        transitionDuration: Duration(milliseconds: 600),
+        transitionDuration: Duration(milliseconds: 300),
         pageBuilder: (context, animation, secondaryAnimation) => widget,
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          return FadeTransition(
-            opacity: animation,
-            child: child,
-          );
+          const begin = Offset(1.0, 0.0); // Mulai dari kanan layar
+          const end = Offset.zero; // Berakhir di posisi normal
+          const curve = Curves.easeInOut; // Efek transisi halus
+
+          var tween =
+              Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+          var offsetAnimation = animation.drive(tween);
+
+          return SlideTransition(position: offsetAnimation, child: child);
         },
       ),
     );
@@ -20,13 +25,18 @@ class AppNavigator {
   static void push(BuildContext context, Widget widget) {
     Navigator.of(context).push(
       PageRouteBuilder(
-        transitionDuration: Duration(milliseconds: 600),
+        transitionDuration: Duration(milliseconds: 300),
         pageBuilder: (context, animation, secondaryAnimation) => widget,
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          return FadeTransition(
-            opacity: animation,
-            child: child,
-          );
+          const begin = Offset(1.0, 0.0);
+          const end = Offset.zero;
+          const curve = Curves.easeInOut;
+
+          var tween =
+              Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+          var offsetAnimation = animation.drive(tween);
+
+          return SlideTransition(position: offsetAnimation, child: child);
         },
       ),
     );
@@ -35,13 +45,18 @@ class AppNavigator {
   static void pushAndRemove(BuildContext context, Widget widget) {
     Navigator.of(context).pushAndRemoveUntil(
       PageRouteBuilder(
-        transitionDuration: Duration(milliseconds: 600),
+        transitionDuration: Duration(milliseconds: 300),
         pageBuilder: (context, animation, secondaryAnimation) => widget,
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          return FadeTransition(
-            opacity: animation,
-            child: child,
-          );
+          const begin = Offset(1.0, 0.0);
+          const end = Offset.zero;
+          const curve = Curves.easeInOut;
+
+          var tween =
+              Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+          var offsetAnimation = animation.drive(tween);
+
+          return SlideTransition(position: offsetAnimation, child: child);
         },
       ),
       (Route<dynamic> route) => false,
