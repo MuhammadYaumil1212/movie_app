9636f8610d1d4d80d6373567747bf938096ad93b
yaumil <yaumilramkhadani@gmail.com>
2025-02-12 17:36:23 +0700

    add animation in snackbar

--- lib/core/helper/message/message.dart
+++ lib/core/helper/message/message.dart
@@ -1,21 +1,26 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DisplayMessage {
-  static void errorMessage(String message, BuildContext context) {
-    var snackbar = SnackBar(
+  static void showMessage(String message, BuildContext context,
+      {Color? backgroundColor}) {
+    final snackbar = SnackBar(
       content: Text(message),
-      backgroundColor: Colors.red,
+      backgroundColor: backgroundColor ?? Colors.green,
       behavior: SnackBarBehavior.floating,
+      duration: Duration(seconds: 2),
+      animation: kAlwaysDismissedAnimation,
     );
+
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 
+  static void errorMessage(String message, BuildContext context) {
+    showMessage(message, context, backgroundColor: Colors.red);
+  }
+
   static void successMessage(String message, BuildContext context) {
-    var snackbar = SnackBar(
-      content: Text(message),
-      behavior: SnackBarBehavior.floating,
-    );
-    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+    showMessage(message, context, backgroundColor: Colors.green);
   }
 }
